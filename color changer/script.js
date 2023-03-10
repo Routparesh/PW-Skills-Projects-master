44862b3741ae77d6b07cd98dc61893c26031a4c8
paresh <routpareshkumar737@gmail.com>
2023-03-10 10:49:30 +0530

    comlete DOM color changer project

---
+++ color changer/script.js
@@ -0,0 +1,16 @@
+'use strict';
+let btn = document.getElementById('btn');
+const randomColor = () => {
+	let val = '0123456789ABCDEF';
+	let cons = '#';
+	for (let i = 0; i < 6; i++) {
+		cons = cons + val[Math.floor(Math.random() * 16)];
+	}
+	return cons;
+};
+
+function changeRandomColor() {
+	document.body.style.backgroundColor = randomColor();
+}
+
+btn.addEventListener('click', changeRandomColor);
